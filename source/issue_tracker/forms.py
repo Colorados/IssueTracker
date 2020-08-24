3b562ee6552560f94c894f12d349d4aded73f200
Seitek <fall3nleaf@gmail.com>
2020-08-24 20:47:30 +0600

    Поле type в форме переделано с использованием ModelMultipleChoiceField

--- source/issue_tracker/forms.py
+++ source/issue_tracker/forms.py
@@ -5,9 +5,9 @@ from .models import Status, Type
 
 class IssueForm(forms.Form):
     summary = forms.CharField(max_length=200, required=True, label='Краткое описание')
-    description = forms.CharField(max_length=1000, required=True, label='Полное описание', widget=forms.Textarea)
+    description = forms.CharField(max_length=1000, required=False, label='Полное описание', widget=forms.Textarea)
     status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Статус', initial='New')
-    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=True, label='Тип')
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=True, label='Тип', widget=forms.SelectMultiple)
     created_at = forms.DateField(required=False,
                                  label='Дата создания',
                                  input_formats=['%Y-%m-%d'],
