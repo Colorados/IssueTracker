eb6cab3b5202f6eb0e7b4ee44a90a2ae072e4a21
Seitek <fall3nleaf@gmail.com>
2020-09-24 17:16:59 +0600

    IndexView переделан с использованием ListView, добавлена пагинация на странице просмотра всех задач и стили пагинации

--- source/issue_tracker/views.py
+++ source/issue_tracker/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseNotAllowed
 from django.urls import reverse
-from django.views.generic import View, TemplateView, FormView
+from django.views.generic import View, TemplateView, FormView, ListView
 from .base_views import FormView as CustomFormView
 from .models import Issues
-from .forms import IssueForm
+from .forms import IssueForm, SimpleSearchForm
+from django.db.models import Q
 
 
-class IndexView(View):
-    def get(self, request):
+class IndexView(ListView):
+    template_name = 'index.html'
+    context_object_name = 'issues'
+    paginate_by = 10
+    paginate_orphans = 0
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        form = SimpleSearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            kwargs['search'] = search
+        return super().get_context_data(object_list=object_list, **kwargs)
+
+    def get_queryset(self):
         data = Issues.objects.all()
-        return render(request, 'index.html', context={'issues': data})
 
+        form = SimpleSearchForm(data=self.request.GET)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            if search:
+                data = data.filter(Q(summary__icontains=search) | Q(description__icontains=search))
+        return data.order_by('-created_at')
 
 class IssueView(TemplateView):
     template_name = 'issue_view.html'
