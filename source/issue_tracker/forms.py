36c0f8d54908361f24cd41a30e11c1800b957a02
Seitek <fall3nleaf@gmail.com>
2020-09-24 17:14:10 +0600

    Добавлена форма SimpleSearchForm в forms.py

--- source/issue_tracker/forms.py
+++ source/issue_tracker/forms.py
@@ -5,8 +5,11 @@ from .models import Issues
 class IssueForm(forms.ModelForm):
     created_at = forms.DateField(required=False, label='Дата создания', input_formats=['%Y-%m-%d'],
                                  widget=forms.DateInput(attrs={'type': 'date'}))
+
     class Meta:
         model = Issues
         fields = ['summary', 'description', 'status', 'types']
 
 
+class SimpleSearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label='Search')
