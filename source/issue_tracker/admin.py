acbee103452ee04c2d1b3bc668fb5f57ab68bacd
Seitek <fall3nleaf@gmail.com>
2020-09-25 21:53:19 +0600

    Модель проекта зарегистрирована в админке

--- source/issue_tracker/admin.py
+++ source/issue_tracker/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from issue_tracker.models import Issues, Status, Type
+from issue_tracker.models import Issues, Status, Type, Project
 
 admin.site.register(Issues)
 admin.site.register(Type)
 admin.site.register(Status)
+admin.site.register(Project)
 
