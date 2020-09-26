dabd7b93349a80b1df2a86592eb51566e35e4f95
Seitek <fall3nleaf@gmail.com>
2020-09-26 11:04:49 +0600

    Добавлено представление для создания пользователя

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from .forms import MyUserCreationForm
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
 
 def login_view(request):
     context = {}
@@ -17,3 +20,22 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'user_create.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('issue_tracker:home')
+        return next_url
